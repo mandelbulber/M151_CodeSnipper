aecf032266d279a5b30882c54f490aca805d31e9
Tim Arnold <tarnold@online.gibz.ch>
2022-05-23 15:33:34 +0200

    Add Language Property to CodeSnippet.cs

--- source/M151_CodeSnipper/CodeSnipper_DAL/Models/CodeSnippet.cs
+++ source/M151_CodeSnipper/CodeSnipper_DAL/Models/CodeSnippet.cs
@@ -15,6 +15,7 @@ namespace CodeSnipper_DAL.Models
         [Required]
         public string Content { get; set; } = string.Empty;
         public bool IsPublic { get; set; }
+        public string Language { get; set; } = string.Empty;
 
         public static List<string> Languages { get; } = new()
         {
