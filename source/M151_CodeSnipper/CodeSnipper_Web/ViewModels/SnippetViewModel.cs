297584a80dc118aea16da0daf714b2ce052aa51c
Andrin Eugster <aeugster@online.gibz.ch>
2022-06-09 20:57:14 +0200

    Add SnippetViewModel

---
+++ source/M151_CodeSnipper/CodeSnipper_Web/ViewModels/SnippetViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace CodeSnipper_Web.ViewModels
+{
+    public class SnippetViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public bool IsPublic { get; set; }
+        public string Language { get; set; }
+
+        public static List<string> Languages { get; } = new()
+        {
+            "C",
+            "C++",
+            "C#",
+            "C# Script",
+            "VB",
+            "Java",
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "PHP",
+            "Python",
+            "Rust",
+            "SQL (General)",
+            "SQL (MySQL)",
+            "SQL (MSSQL)"
+        };
+    }
+}
