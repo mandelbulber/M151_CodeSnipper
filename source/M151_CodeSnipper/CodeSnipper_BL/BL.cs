b1a3aaf2e1be49dbdb50f4ae72502b88ca817208
Tim Arnold <tarnold@online.gibz.ch>
2022-06-13 18:20:18 +0200

    Implement functionality for editing snippets

--- source/M151_CodeSnipper/CodeSnipper_BL/BL.cs
+++ source/M151_CodeSnipper/CodeSnipper_BL/BL.cs
@@ -12,5 +12,25 @@ namespace CodeSnipper_BL
             _dbContext.CodeSnippets.Add(codeSnippet);
             _dbContext.SaveChanges();
         }
+
+        public static CodeSnippet? GetCodeSnippet(string id)
+        {
+            CodeSnippet? codeSnippet = _dbContext.CodeSnippets.FirstOrDefault(x => x.Id == id);
+            return codeSnippet;
+        }
+
+        public static void UpdateSnippet(CodeSnippet codeSnippet)
+        {
+            CodeSnippet? dbCodeSnippet = _dbContext.CodeSnippets.FirstOrDefault(x => x.Id == codeSnippet.Id);
+            if (dbCodeSnippet == null)
+                return;
+
+            dbCodeSnippet.Title = codeSnippet.Title;
+            dbCodeSnippet.Content = codeSnippet.Content;
+            dbCodeSnippet.Language = codeSnippet.Language;
+            dbCodeSnippet.IsPublic = codeSnippet.IsPublic;
+
+            _dbContext.SaveChanges();
+        }
     }
 }
---
+++ source/M151_CodeSnipper/CodeSnipper_Web/Controllers/EditController.cs
@@ -0,0 +1,55 @@
+﻿using CodeSnipper_BL;
+using CodeSnipper_DAL.Models;
+using CodeSnipper_Web.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace CodeSnipper_Web.Controllers
+{
+    [Authorize]
+    public class EditController : Controller
+    {
+        [HttpGet]
+        [Route("Edit/{id}")]
+        public IActionResult Index(string id)
+        {
+            CodeSnippet? codeSnippet = BL.GetCodeSnippet(id);
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (codeSnippet == null || codeSnippet.OwnerId != userId)
+                return View("Error", id);
+
+            SnippetViewModel snippetViewModel = new()
+            {
+                Id = codeSnippet.Id,
+                Title = codeSnippet.Title,
+                Content = codeSnippet.Content,
+                IsPublic = codeSnippet.IsPublic,
+                Language = codeSnippet.Language
+            };
+
+            return View(snippetViewModel);
+        }
+
+        [HttpPost]
+        [Route("Edit/{id}")]
+        public IActionResult Index(SnippetViewModel snippetViewModel)
+        {
+            CodeSnippet? dbCodeSnippet = BL.GetCodeSnippet(snippetViewModel.Id);
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (dbCodeSnippet == null || dbCodeSnippet.OwnerId != userId)
+                return RedirectToAction("Index", "Home");
+
+            dbCodeSnippet.Title = snippetViewModel.Title;
+            dbCodeSnippet.Content = snippetViewModel.Content;
+            dbCodeSnippet.IsPublic = snippetViewModel.IsPublic;
+            dbCodeSnippet.Language = snippetViewModel.Language;
+
+            BL.UpdateSnippet(dbCodeSnippet);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
