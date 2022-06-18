68fdeebb206c34ecf35d39ca7fd92503bd707613
Andrin Eugster <aeugster@online.gibz.ch>
2022-06-19 01:07:20 +0200

    Create MySnippets controller & view

---
+++ source/M151_CodeSnipper/CodeSnipper_Web/Controllers/MySnippets.cs
@@ -0,0 +1,33 @@
+﻿using CodeSnipper_BL;
+using CodeSnipper_DAL.Models;
+using CodeSnipper_Web.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace CodeSnipper_Web.Controllers
+{
+    [Authorize]
+    public class MySnippets : Controller
+    {
+        public IActionResult Index()
+        {
+            List<CodeSnippet> dbSnippets = BL.GetUsersCodeSnippets(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            List<SnippetViewModel> snippetViewModels = new();
+            foreach (CodeSnippet codeSnippet in dbSnippets)
+            {
+                SnippetViewModel snippetViewModel = new()
+                {
+                    Id = codeSnippet.Id,
+                    Title = codeSnippet.Title,
+                    Content = codeSnippet.Content,
+                    OwnerId = codeSnippet.OwnerId,
+                    Language = codeSnippet.Language,
+                    IsPublic = codeSnippet.IsPublic
+                };
+                snippetViewModels.Add(snippetViewModel);
+            }
+            return View(snippetViewModels);
+        }
+    }
+}
