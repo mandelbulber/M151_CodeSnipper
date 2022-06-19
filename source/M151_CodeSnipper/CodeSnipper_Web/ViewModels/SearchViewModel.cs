7a9f73bce73cb54ec3f930d1cad32c71acef320c
Andrin Eugster <aeugster@online.gibz.ch>
2022-06-19 17:32:14 +0200

    Create SearchViewModel

---
+++ source/M151_CodeSnipper/CodeSnipper_Web/ViewModels/SearchViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeSnipper_Web.ViewModels
+{
+    public class SearchViewModel
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Language { get; set; } = string.Empty;
+        public List<SnippetViewModel> Snippets { get; set; } = new();
+    }
+}
