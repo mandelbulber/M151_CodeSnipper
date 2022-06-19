8a7245c7466fdaf4ec762a8ad4e81fd878fc7a6b
Andrin Eugster <aeugster@online.gibz.ch>
2022-06-19 17:32:47 +0200

    Create SearchController & View

---
+++ source/M151_CodeSnipper/CodeSnipper_Web/Controllers/SearchController.cs
@@ -0,0 +1,28 @@
+﻿using CodeSnipper_BL;
+using CodeSnipper_DAL.Models;
+using CodeSnipper_Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace CodeSnipper_Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index(string language, string title)
+        {
+            List<CodeSnippet> dbSnippets = BL.GetAllAvailableSnippets(User.FindFirstValue(ClaimTypes.NameIdentifier))
+                .Where(x => (language == "All" || x.Language == language) && (title == null || x.Title.ToUpper().Contains(title.ToUpper())))
+                .ToList();
+            SearchViewModel searchViewModel = new()
+            {
+                Language = language,
+                Title = title
+            };
+            foreach (CodeSnippet codeSnippet in dbSnippets)
+            {
+                searchViewModel.Snippets.Add(codeSnippet.ConvertToSnippetViewModel());
+            }
+            return View(searchViewModel);
+        }
+    }
+}
