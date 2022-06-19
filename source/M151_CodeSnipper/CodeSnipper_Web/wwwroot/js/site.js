5abb6e8cdc8976796205ebb22bcc863d9e69c968
Tim Arnold <tarnold@online.gibz.ch>
2022-06-19 15:57:45 +0200

    Fix copy function

--- source/M151_CodeSnipper/CodeSnipper_Web/wwwroot/js/site.js
+++ source/M151_CodeSnipper/CodeSnipper_Web/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(document).ready(function ($) {
+    $(".copyContentButton").click(function () {
+        let text = $(this).prev().prev().prev().val();
+        navigator.clipboard.writeText(text);
+    })
+});
